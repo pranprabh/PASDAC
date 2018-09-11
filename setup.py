eeb91c5076075c5549abe2c0f01745ee4e0fa2d6
abitoflachemy <salvador.aguinaga@gmail.com>
2018-09-10 19:54:07 -0500

    creating package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='PASDAC',
+	version='0.1.dev',
+	packages=['PASDAC',],
+	url='https://github.com/HAbitsLab/PASDAC',
+	description='Time Series Classification Pipeline',
+	author='Nabil Alshurafa',
+	author_email='nabil@',
+	license='Creative Commons Attribution-Noncommercial-Share Alike license',
+	long_description=open('README.md').read(),
+
+)
