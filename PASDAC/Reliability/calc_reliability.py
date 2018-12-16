4ac48edd8309cc175dc944058a94587d0fa39d66
Sal Aguinaga <sal.aguinaga@northwestern.edu>
2018-12-16 10:56:00 -0500

    manual merge with Shibos feature branch

--- PASDAC/Reliability/calc_reliability.py
+++ PASDAC/Reliability/calc_reliability.py
@@ -1,23 +1,23 @@
 """
-Function:
+    Function:
     calc_reliability(timeArr, unit, plot=0)
-
+    
     : Calculate the reliability of time series data from sensor.
-
-Requirement: 
+    
+    Requirement:
     package: python(either python2.x or python 3.x), pandas, numpy, matplotlib
     The time must be unixtimestamp in milliseconds.
-
-Development log:
+    
+    Development log:
     1. function and script name the same - done
     2. options for second, minute, hour - done
     3. modification: more comments - done
-    4. (add y label in figure. - no figure saving) plot or not, to add one param y
+    4. (add y label in figure. - no figure saving) plot or not, to add one param y - done
     5. non-idle -> hasData - done
     6. add plot switch: reliability_calc(timeArr, unit, plot=0) - done
     7. another script named 'save_reliability.py' - done
-
-"""
+    
+    """
 
 
 from __future__ import division
@@ -28,15 +28,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def calc_reliability(timeArr, sensorFreq, unit, plot=0):
+def calc_reliability(timeArr, unit='second', plot=0):
     """
-    Calculate the reliability of time series data from sensor.
-    Requirement: unixtimestamp must be in milliseconds.
-    :param timeArr: time array of unixtimestamp in milliseconds, size N*1
-    :param unit: str, options: "second", "minute", "hour"
-    :return countDf: reliability result dataframe with columns 'Time' and 'SampleCounts'.
-    """
-
+        Calculate the reliability of time series sensor data in each 'unit' from the start of the 'timeArr' to the end.
+        Plot is optional.
+        
+        Requirement: timeArr must be unixtimestamp in milliseconds.
+        
+        :param timeArr: time array of unixtimestamp in milliseconds, size N*1
+        :param unit: str, options: "second", "minute", "hour"
+        :param plot: 0 or 1
+        :return countDf: reliability result dataframe with columns 'Time' and 'SampleCounts'.
+        
+        """
+    
     # ==================================================================================
     # generate the reliability dataframe
     # ==================================================================================
@@ -45,7 +50,7 @@ def calc_reliability(timeArr, sensorFreq, unit, plot=0):
         "minute": 60000,
         "hour": 3600000,
     }
-
+    
     timeNoDuplicateArr = np.unique(timeArr)
     timeNoDuplicateArr = np.floor_divide(timeNoDuplicateArr, msecCnts[unit]).astype(int)
     timeNoDuplicateArr = np.sort(timeNoDuplicateArr)
@@ -53,7 +58,7 @@ def calc_reliability(timeArr, sensorFreq, unit, plot=0):
     reliabilityTimeList = []
     reliabilitySampleCountsList = []
     count = 0
-
+    
     # loop through the timeNoDuplicateArr
     for i in range(len(timeNoDuplicateArr) - 1):
         # if next timestamp is the same as current one
@@ -70,23 +75,77 @@ def calc_reliability(timeArr, sensorFreq, unit, plot=0):
                 # append 0 to noData seconds
                 reliabilitySampleCountsList.append(0)
 
-    reliabilityTimeList.append(timeNoDuplicateArr[-1])
-    reliabilitySampleCountsList.append(count + 1)
-    countDf = pd.DataFrame({'Time':reliabilityTimeList,'SampleCounts':reliabilitySampleCountsList},\
-                            columns=['Time','SampleCounts'])
+    # With try-except, no need to check the input and the empty countDf will be returned.
+    # Advantage: In batch processing, when empty data files with only header exist,
+    #  the reliability files will follow the same pattern.
+    try:
+        reliabilityTimeList.append(timeNoDuplicateArr[-1])
+        reliabilitySampleCountsList.append(count + 1)
+        except:
+            print('Warning: timeArr is empty!')
 
-    # ==================================================================================
-    # plot figure
-    # ==================================================================================
-    if plot:
-        countDf['Time'] = pd.to_datetime(countDf['Time'], unit='s', utc=True)
-        countDf = countDf.set_index(['Time'])
-        countDf.index = countDf.index.tz_convert('US/Central')
+        countDf = pd.DataFrame({'Time':reliabilityTimeList,'SampleCounts':reliabilitySampleCountsList},\
+                               columns=['Time','SampleCounts'])
 
-        f = plt.figure(figsize=(12,5))
+# ==================================================================================
+# plot figure
+# ==================================================================================
+if plot:
+    countDf['Time'] = pd.to_datetime(countDf['Time'], unit='s', utc=True)
+    countDf = countDf.set_index(['Time'])
+    countDf.index = countDf.index.tz_convert('US/Central')
+    
+    f = plt.figure(figsize=(12,5))
         countDf.plot(style=['b-'], ax=f.gca())
-        plt.title('Frequency')
+        plt.title('Reliability Test')
+        plt.ylabel('Count Per Unit')
         plt.show()
-        # plt.savefig(os.path.join(outfolder, 'reliability(frequency).png')) # FYI, save fig function
-
+    # plt.savefig(os.path.join(outfolder, 'reliability(frequency).png')) # FYI, save fig function
+    
     return countDf
+
+
+def test_case():
+    filePath = '08-24-17_08.csv'
+    timestampCol = 1
+    saveFolder = 'second'
+    unit = 'second'
+    
+    if not os.path.exists(saveFolder):
+        os.makedirs(saveFolder)
+    try:
+        df = pd.read_csv(filePath)
+        timeArr = df.iloc[:,timestampCol-1].values
+    except Exception:
+        print("Data file {} does not exists.".format(filePath))
+        exit()
+    
+    # requirement: unixtimestamp must be in milliseconds
+    countDf = calc_reliability(timeArr, unit, plot=1)
+    print(countDf)
+
+
+def call_from_cmd_line():
+    # get the file path where the sampled data and timestamp are saved
+    filePath = str(sys.argv[1])
+    # specify the path where plot and count-per-second file will be saved.
+    saveFolder = str(sys.argv[2])
+    # get the column of timestamp in data file
+    timestampCol = int(sys.argv[3])
+    
+    if not os.path.exists(saveFolder):
+        os.makedirs(saveFolder)
+    try:
+        df = pd.read_csv(filePath)
+        timeArr = df.iloc[:,timestampCol-1].values
+    except Exception:
+        print("Data file {} does not exists.".format(filePath))
+        exit()
+    
+    # Requirement: timeArr must be unixtimestamp in milliseconds.
+    countDf = calc_reliability(timeArr, unit, plot=0)
+
+
+if __name__ == "__main__":
+    test_case()
+
--- PASDAC/Reliability/score_reliability.py
+++ PASDAC/Reliability/score_reliability.py
@@ -1,43 +1,41 @@
 """
-Function:
+    Function:
     score_reliability(countDf, sensorFreq=20, unit='second')
-
+    
     : Calculate and print out the reliability score.
-
-Requirement: 
+    
+    Requirement:
     package: python(either python2.x or python 3.x), pandas, numpy, matplotlib
     The time must be unixtimestamp in milliseconds.
-
-"""
+    
+    """
 
 from __future__ import division
 import os
 import sys
 import numpy as np
 import pandas as pd
-from calc_reliability import calc_reliability 
+from calc_reliability import calc_reliability
 
 
-def score_reliability(countDf, sensorFreq, unit):
+def score_reliability(countDf, sensorFreq, unit='second'):
     """
-    :param countDf:
-    :param sensorFreq:
-    :param unit:
-    :return: None
-    """
-
-    # # ==================================================================================
-    # # save reliability 
-    # # (This is a simple one-line command so it should be a seperate function - Shibo)
-    # # ==================================================================================
-    # countDf.to_csv(os.path.join(outfolder, 'reliability.csv'), index=False)
+        Calculate and print out the reliability score.
+        
+        :param countDf: the dataframe of ['Time','SampleCounts'], which is the return of function calc_reliability
+        :param sensorFreq: in Hz
+        :param unit: str, options: "second", "minute", "hour"
+        :return: reliabilityHasDataUnits: the average reliability score for the units that has data
+        :return: reliability: the average reliability score for all the units
+        """
+    
     # ==================================================================================
     # print out reliability score for has-data units and for all units
     # ==================================================================================
     
     countDf.index = pd.to_datetime(countDf.index, unit='s', utc=True)
     print("Duration: {}".format(countDf.index[-1] - countDf.index[0]))
-
+    
     idealCntInUnits = {
         "second": sensorFreq,
         "minute": sensorFreq*60,
@@ -55,7 +53,62 @@ def score_reliability(countDf, sensorFreq, unit):
     samplingFreqArr = np.full_like(countArr, idealCntInUnits[unit])
     countArr = np.minimum(countArr, samplingFreqArr)
     reliability = np.sum(countArr)/np.sum(samplingFreqArr)
-
+    
     print("Reliability for all the units(seconds or minutes or hours): {}".format(reliability))
-
+    
     return reliabilityHasDataUnits, reliability
+
+
+def test_case():
+    filePath = '08-24-17_08.csv'
+    timestampCol = 1
+    saveFolder = 'minute'
+    unit = 'minute'
+    sensorFreq = 20
+    
+    if not os.path.exists(saveFolder):
+        os.makedirs(saveFolder)
+    try:
+        df = pd.read_csv(filePath)
+        timeArr = df.iloc[:,timestampCol-1].values
+    except Exception:
+        print("Data file {} does not exists.".format(filePath))
+        exit()
+
+    # requirement: unixtimestamp must be in milliseconds
+    countDf = calc_reliability(timeArr, sensorFreq, unit, plot=0)
+    countDf.to_csv(os.path.join(saveFolder, 'reliability.csv'), index=False)
+
+    score_reliability(countDf, sensorFreq, unit)
+
+
+def call_from_cmd_line():
+    # get the file path where the sampled data and timestamp are saved
+    filePath = str(sys.argv[1])
+    # specify the path where plot and count-per-second file will be saved.
+    saveFolder = str(sys.argv[2])
+    # get the column of timestamp in data file
+    timestampCol = int(sys.argv[3])
+    # get the sensor frequency setting
+    sensorFreq = int(sys.argv[4])
+    
+    
+    if not os.path.exists(saveFolder):
+        os.makedirs(saveFolder)
+    try:
+        df = pd.read_csv(filePath)
+        timeArr = df.iloc[:,timestampCol-1].values
+    except Exception:
+        print("Data file {} does not exists.".format(filePath))
+        exit()
+    
+    # requirement: unixtimestamp must be in milliseconds
+    countDf = calc_reliability(timeArr, sensorFreq, unit, plot=0)
+    countDf.to_csv(os.path.join(saveFolder, 'reliability.csv'), index=False)
+
+    score_reliability(countDf, sensorFreq, unit)
+
+
+if __name__ == "__main__":
+    test_case()
+
